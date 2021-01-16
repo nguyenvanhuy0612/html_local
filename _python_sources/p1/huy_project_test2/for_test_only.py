ed9298b00f7cb6853b9a1fd3fa8622090dfcd955
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-01-16 17:33:32 +0700

    ok

--- _python_sources/p1/huy_project_test2/for_test_only.py
+++ _python_sources/p1/huy_project_test2/for_test_only.py
@@ -1,3 +1,4 @@
+# importing csv module
 import csv
 
 
@@ -13,5 +14,102 @@ def write_to_csv():
 
 def temp():
     print(['Spam'] * 5 + ['Baked Beans'])
-temp()
-# write_to_csv()
+
+
+def reading_to_a_csv_file_python():
+    # csv file name
+    filename = "university_records_dict.csv"
+
+    # initializing the titles and rows list
+    fields = []
+    rows = []
+
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+            # get total number of rows
+        print("Total no. of rows: %d" % (csvreader.line_num))
+
+
+    # printing the field names
+    print('Field names are:' + ', '.join(field for field in fields))
+
+    #  printing first 5 rows
+    print('\nFirst 5 rows are:\n')
+    for row in rows[:5]:
+        # parsing each column of a row
+        for col in row:
+            print("%10s" % col),
+            print('\n')
+        pass
+
+    row1 = rows[0:2]
+    print(row1)
+    print(fields)
+
+
+def writing_to_a_csv_file_python():
+    # field names
+    fields = ['Name', 'Branch', 'Year', 'CGPA']
+
+    # data rows of csv file
+    rows = [['Nikhil', 'COE', '2', '9.0'],
+            ['Sanchit', 'COE', '2', '9.1'],
+            ['Aditya', 'IT', '2', '9.3'],
+            ['Sagar', 'SE', '1', '9.5'],
+            ['Prateek', 'MCE', '3', '7.8'],
+            ['Sahil', 'EP', '2', '9.6']]
+
+    # name of csv file
+    filename = "university_records.csv"
+
+    # writing to csv file
+    with open(filename, 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+
+        # writing the fields
+        csvwriter.writerow(fields)
+
+        # writing the data rows
+        csvwriter.writerows(rows)
+
+
+def writing_a_dictionary_to_a_csv_file_python():
+    # my data rows as dictionary objects
+    mydict = [{'branch': 'COE', 'cgpa': '9.0', 'name': 'Nikhil', 'year': '2'},
+              {'branch': 'COE', 'cgpa': '9.1', 'name': 'Sanchit', 'year': '2'},
+              {'branch': 'IT', 'cgpa': '9.3', 'name': 'Aditya', 'year': '2'},
+              {'branch': 'SE', 'cgpa': '9.5', 'name': 'Sagar', 'year': '1'},
+              {'branch': 'MCE', 'cgpa': '7.8', 'name': 'Prateek', 'year': '3'},
+              {'branch': 'EP', 'cgpa': '9.1', 'name': 'Sahil', 'year': '2'}]
+
+    # field names
+    fields = ['name', 'branch', 'year', 'cgpa']
+
+    # name of csv file
+    filename = "university_records_dict.csv"
+
+    # writing to csv file
+    with open(filename, 'a') as csvfile:
+        # creating a csv dict writer object
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        # writing headers (field names)
+        writer.writeheader()
+
+        # writing data rows
+        writer.writerows(mydict)
+
+
+# writing_a_dictionary_to_a_csv_file_python()
+reading_to_a_csv_file_python()
