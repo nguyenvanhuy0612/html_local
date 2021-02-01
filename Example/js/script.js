ee81696c38b2ac2164f4b99846b1b91973049b34
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-02-01 23:49:57 +0700

    upload

--- Example/js/script.js
+++ Example/js/script.js
@@ -1,1 +1,22 @@
-var x = "Hello JavaScript!";
+// var x = "Hello JavaScript!";
+var message = "in global";
+console.log("global: message = " + message);
+
+var a = function(){
+    var message = "inside a";
+    console.log("a: message = " + message);
+    b();
+    c();
+    function c () {
+        console.log("c: message = " + message)
+    }
+}
+
+function b () {
+    console.log("b: message = " + message);
+}
+
+var d = () => {console.log("d hello"); console.log("d world");} //arrow func
+
+a();
+d();
