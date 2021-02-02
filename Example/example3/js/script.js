655c92078e120c47ac98ccd46dd4912de67c03b3
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-02-02 23:03:48 +0700

    add

--- Example/example3/js/script.js
+++ Example/example3/js/script.js
@@ -22,13 +22,16 @@ var social_media = {
     fb : "huy loh",
     google : "nguyen huy",
     github : "huy",
-    "huy": "nguyen"
+    "huy": "nguyen",
+    10: "Abc"
 };
 uname = "huy";
 console.log(social_media);
 console.log(social_media.fb);
-console.log(social_media.google);
+console.log(social_media.google); // truy cap object theo kieu dau cham .
+console.log(social_media[10]); //truy cap theo kieu dau ngoac don []
 console.log(uname);
+console.log(social_media.abc) // neu thuoc tinh k ton tai thi tra ve undefined
 fish = {
     length:20,
     width:7,
---
+++ Example/example4/js/script.js
@@ -0,0 +1,8 @@
+var string = "Hello";
+string += " World";
+// string = string + " World";
+console.log(string + " !");
+
+// Regular math operators: +, -, *, /
+console.log((5 + 4) / 3);
+console.log(undefined / 5);
