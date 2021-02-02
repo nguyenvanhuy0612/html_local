999293015c15532ccb540df91f323776c108136d
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-02-02 22:36:34 +0700

    ok

--- Example/example2/js/script.js
+++ Example/example2/js/script.js
@@ -6,3 +6,20 @@ console.log(x);
 if (x == undefined){
     console.log("x is undefined");
 }
+
+x = 5;
+if (x== undefined){
+    console.log("x is undefined");
+} else {
+    console.log("x has been defined");
+}
+social_media = {
+    "fb" : "huy loh",
+    "google" : "nguyen huy",
+    "github" : "huy"
+}
+uname = "huy";
+console.log(social_media);
+console.log(social_media.fb);
+console.log(social_media.google);
+console.log(uname);
