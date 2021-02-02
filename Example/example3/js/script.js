f3360cab03ba1433d679a9339abe1709b99e1de0
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-02-02 22:51:06 +0700

    changed structure

---
+++ Example/example3/js/script.js
@@ -0,0 +1,39 @@
+
+//should be undefined
+
+/* comment
+123
+ */
+
+ var x;
+console.log(x);
+
+if (x == undefined){
+    console.log("x is undefined");
+}
+
+x = 5;
+if (x== undefined){
+    console.log("x is undefined");
+} else {
+    console.log("x has been defined");
+}
+var social_media = {
+    fb : "huy loh",
+    google : "nguyen huy",
+    github : "huy",
+    "huy": "nguyen"
+};
+uname = "huy";
+console.log(social_media);
+console.log(social_media.fb);
+console.log(social_media.google);
+console.log(uname);
+fish = {
+    length:20,
+    width:7,
+    age:3
+
+};
+
+console.log(fish);
