ef8b14b886af4fb78b89c597e2d7c752a7f83574
Huy Nguyen <nguyenvanhuy0612@gmail.com>
2021-02-03 22:25:56 +0700

    Update

--- Example/example4/js/script.js
+++ Example/example4/js/script.js
@@ -6,3 +6,10 @@ console.log(string + " !");
 // Regular math operators: +, -, *, /
 console.log((5 + 4) / 3);
 console.log(undefined / 5);
+function test(a){
+    console.log(a / 5);
+}
+console.log("invoke test() with no arguments");
+test();
+console.log("invoke test() with an argument");
+test(10);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # html_local
+https://code.visualstudio.com/docs/getstarted/keybindings
+https://letskodeit.teachable.com/
+https://www.tutorialspoint.com/selenium_webdriver_with_python_3_x_novice_to_ninja/index.asp
